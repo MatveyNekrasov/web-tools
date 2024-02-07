ae3ce3cbf6bd84bc562ee3762e6d00449a4c1618
Matvey Nekrasov <matvey1234567@yandex.ru>
2024-02-07 22:39:42 +0300

    create before-after image slider

---
+++ scripts/before-after slider.js
@@ -0,0 +1,86 @@
+"use strict";
+
+const slider = document.querySelector(".before-after-slider");
+const before = document.querySelector(".before-img");
+const beforeImage = document.querySelector(".slider-img");
+const resizer = document.querySelector(".resizer");
+
+let isActive = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+  debugger;
+  let width = slider.offsetWidth
+    ? slider.offsetWidth
+    : document.forms["image-convertor-form"].offsetWidth;
+  beforeImage.style.width = width + "px";
+});
+
+window.addEventListener("resize", () => {
+  let width = slider.offsetWidth;
+  beforeImage.style.width = width + "px";
+});
+
+resizer.addEventListener("mousedown", () => {
+  isActive = true;
+  resizer.classList.add("resize");
+});
+
+document.body.addEventListener("mouseup", () => {
+  isActive = false;
+  resizer.classList.remove("resize");
+});
+
+document.body.addEventListener("mouseleave", () => {
+  isActive = false;
+  resizer.classList.remove("resize");
+});
+
+document.body.addEventListener("mousemove", (evt) => {
+  if (!isActive) return;
+  let x = evt.pageX;
+  x -= slider.getBoundingClientRect().left;
+  slideIt(x);
+  pauseEvent(evt);
+});
+
+resizer.addEventListener("touchstart", () => {
+  isActive = true;
+  resizer.classList.add("resize");
+});
+
+document.body.addEventListener("touchend", () => {
+  isActive = false;
+  resizer.classList.remove("resize");
+});
+
+document.body.addEventListener("touchcancel", () => {
+  isActive = false;
+  resizer.classList.remove("resize");
+});
+
+document.body.addEventListener("touchmove", (evt) => {
+  if (!isActive) return;
+  let x;
+
+  for (let i = 0; i < evt.changedTouches.length; i++) {
+    x = evt.changedTouches[i].pageX;
+  }
+
+  x -= slider.getBoundingClientRect().left;
+  slideIt(x);
+  pauseEvent(evt);
+});
+
+function slideIt(x) {
+  let transform = Math.max(0, Math.min(x, slider.offsetWidth));
+  before.style.width = transform + "px";
+  resizer.style.left = transform - 0 + "px";
+}
+
+function pauseEvent(evt) {
+  if (evt.stopPropagation) evt.stopPropagation();
+  if (evt.preventDefault) evt.preventDefault();
+  evt.cancelBubble = true;
+  FocusEvent.returnValue = false;
+  return false;
+}
--- scripts/image-convertor.js
+++ scripts/image-convertor.js
@@ -13,7 +13,10 @@ imageConvertorForm.addEventListener("submit", async (evt) => {
   const resultImageExtension =
     imageConvertorForm.elements["image-convertor__file-extensions"].value;
 
-  const resultImage = await sendImageToConvert(formData, resultImageExtension);
+  const resultImage = /* await */ sendImageToConvert(
+    formData,
+    resultImageExtension
+  );
 
   createResultContainer(
     document.querySelector(".tools__image-convertor__section"),
@@ -28,8 +31,14 @@ imageConvertorForm.addEventListener("submit", async (evt) => {
 
   const resultImageElement = document.createElement("img");
   resultImageElement.classList.add(".result-img");
-  resultImageElement.src = URL.createObjectURL(resultImage);
+  /* resultImageElement.src = URL.createObjectURL(resultImage); */
   document.querySelector(".result").append(resultImageElement);
+
+  createSlider(
+    fileValue,
+    fileValue,
+    document.querySelector(".before-after-slider")
+  );
 });
 
 async function sendImageToConvert(data, resultImageExtension) {
@@ -49,5 +58,28 @@ async function sendImageToConvert(data, resultImageExtension) {
     return await response.blob();
   } catch (err) {
     console.log(err);
+  }
+}
+
+function createSlider(sourceImage, resultImage, containerElement) {
+  const beforeImage = containerElement.querySelector(".before-img > img");
+  const afterImage = containerElement.querySelector(".after-img > img");
+  let beforeFileReader = new FileReader();
+  let afterFileReader = new FileReader();
+  beforeFileReader.onload = (evt) =>
+    handleFileReaderLoad(evt, beforeImage, containerElement);
+  afterFileReader.onload = (evt) =>
+    handleFileReaderLoad(evt, afterImage, containerElement);
+  beforeFileReader.readAsDataURL(sourceImage);
+  afterFileReader.readAsDataURL(resultImage);
+}
+
+function handleFileReaderLoad(evt, imageElement, containerElement) {
+  imageElement.src = evt.target.result;
+  if (
+    evt.target.readyState === 2 &&
+    !containerElement.classList.contains("is-shown")
+  ) {
+    containerElement.classList.add("is-shown");
   }
 }
